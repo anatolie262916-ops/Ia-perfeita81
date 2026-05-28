a5f21e91b53e5765ab7c0791f8c731f3168ca4a4
anatolie262916-ops <anatolie262916@gmail.com>
2026-05-28 16:16:35 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,29 +1,20 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TELEGRAM_TOKEN =8346701498:AAHRz3zckfQlkHVTFUdQrimvOzjxe5PCf_U
+TOKEN = 8346701498:AAHRz3zckfQlkHVTFUdQ-rimvOzjxe5PCf_U
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥 IA EXTREMA ONLINE!")
+    await update.message.reply_text("Bot online!")
 
 async def sinais(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    texto = """
-🚨 IA EXTREMA 🚨
+    await update.message.reply_text(
+        "⚽ Benfica vs Porto\n🎯 Empate\n📈 78%"
+    )
 
-⚽ Benfica vs Porto
-🎯 Empate
-📈 Confiança: 78%
-
-⚽ Arsenal vs Chelsea
-🎯 Vitória Arsenal
-📈 Confiança: 83%
-"""
-    await update.message.reply_text(texto)
-
-app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("sinais", sinais))
 
-print("Bot online...")
+print("Bot online")
 app.run_polling()
