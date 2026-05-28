5052d7df9307967c4ac5054eaa3e28b59148f310
anatolie262916-ops <anatolie262916@gmail.com>
2026-05-28 16:41:59 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8346701498:AAHRz3zckfQlkHVTFUdQ-rimvOzjxe5PCf_U"
+TOKEN = "8346701498:AAFgFEzEHRx1NBZo5IUWPocW3dI31JzUqMo"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot online!")
